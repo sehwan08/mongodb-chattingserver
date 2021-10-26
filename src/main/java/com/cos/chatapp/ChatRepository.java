ad911da14318fbd25bb110950b331f684eaa10be
sehwan08 <sehwan_8@naver.com>
2021-10-26 21:20:30 +0900

    Server settings

---
+++ src/main/java/com/cos/chatapp/ChatRepository.java
@@ -0,0 +1,14 @@
+package com.cos.chatapp;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+	
+	@Tailable //커서를 닫지않고 계속 유지
+	@Query("{sender:?0, receiver:?1}")
+	Flux<Chat> mFindBySender(String sender, String receiver);
+}
